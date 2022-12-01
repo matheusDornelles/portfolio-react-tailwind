a975b81c252b12e20e2e7293736034210b39423a
Matheus Dornelles <mdmaia91@gmail.com>
2022-12-01 00:27:06 -0300

    fixed navbar links

--- portfolio-tailwind/src/components/NavBar.jsx
+++ portfolio-tailwind/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png'
+import { Link } from 'react-scroll'
 
 const NavBar = () => {
 
@@ -18,12 +19,26 @@ const NavBar = () => {
       {/* menu*/}
 
       <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Experience</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>
+          <Link to="home" smooth={true} duration={500} >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500} >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500} >
+            Skills
+          </Link>
+        </li>
+        <li>
+        <Link  to="contact"  smooth={true}  duration={500} >
+          Contact
+        </Link>
+        </li>
       </ul>
       {/* Hamburguer */}
       <div onClick={handleClick} className='md:hidden z-10'>
@@ -37,33 +52,47 @@ const NavBar = () => {
             ? 'hidden'
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }>
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Experience</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'>
+        <Link onClick={handleClick} to="home" smooth={true} duration={500} >
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+        <Link onClick={handleClick} to="about" smooth={true} duration={500} >
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+        <Link onClick={handleClick} to="skills" smooth={true} duration={500} >
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+        <Link onClick={handleClick}  to="contact"  smooth={true}  duration={500} >
+          Contact
+        </Link>
+        </li>
       </ul>
 
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] font-bold font-sans text-lg hover:ml-[-10px] duration-300'>
-            <a className='flex justify-between items-center w-full text-gray-300' 
-            href="https://www.linkedin.com/in/matheus-dornelles-02942791/">
-              Linkedin<FaLinkedin size={30}/>
+            <a className='flex justify-between items-center w-full text-gray-300'
+              href="https://www.linkedin.com/in/matheus-dornelles-02942791/">
+              Linkedin<FaLinkedin size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] font-bold font-sans text-lg hover:ml-[-10px] duration-300'>
-            <a className='flex justify-between items-center w-full text-gray-300' 
-            href="https://github.com/matheusDornelles">
-              Github<FaGithub size={30}/>
+            <a className='flex justify-between items-center w-full text-gray-300'
+              href="https://github.com/matheusDornelles">
+              Github<FaGithub size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] font-bold font-sans text-lg hover:ml-[-10px] duration-300'>
-            <a className='flex justify-between items-center w-full text-gray-300' 
-            href="/">
-              Email<HiOutlineMail size={30}/>
+            <a className='flex justify-between items-center w-full text-gray-300'
+              href="/">
+              Email<HiOutlineMail size={30} />
             </a>
           </li>
         </ul>
