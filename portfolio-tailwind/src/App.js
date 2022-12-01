52ec1f23a98ccbf991c8e7a2d8a5fe3f68dfc69d
Matheus Dornelles <mdmaia91@gmail.com>
2022-12-01 00:08:44 -0300

    Contact Finished

--- portfolio-tailwind/src/App.js
+++ portfolio-tailwind/src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Skills from "./components/Skills";
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Contact />
     </div>
     
   );
--- portfolio-tailwind/src/components/NavBar.jsx
+++ portfolio-tailwind/src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div >
-        <img src={Logo} alt="Logo image" style={{ width: '50px' }} />
+        {/*<img src={Logo} alt="Logo image" style={{ width: '50px' }} />*/}
       </div>
 
       {/* menu*/}
