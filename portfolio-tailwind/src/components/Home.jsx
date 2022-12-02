b218a7900ad64baac6d48db62f19b46d504a7977
Matheus Dornelles <mdmaia91@gmail.com>
2022-12-01 23:12:17 -0300

    fixed name on home

--- portfolio-tailwind/src/components/Home.jsx
+++ portfolio-tailwind/src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
             {/*container*/}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className='text-pink-600'>Olá, meu name é</p>
+                <p className='text-pink-600'>Olá, meu nome é</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Matheus Dornelles</h1>
                 <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Eu sou um Full Stack Developer</h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]  '>eu sou um desenvolvedor full-stack especializado em construir sistemas web com REACT, estou focado em
